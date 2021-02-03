99421a2f751459eec32f8a0b8083d50231ed94e4
Sarang <sarang13579@gmail.com>
2021-02-03 16:11:14 +0530

    Filter method
    Filter method

--- functional_js_syntax/array_filter.js
+++ functional_js_syntax/array_filter.js
@@ -1,13 +1,24 @@
 // 1. Find all the words with more than 7 characters
-const words = ['tardis', 'grok', 'frak', 'blaster', 'klingon', 'shepherd']
+const words1 = ['tardis', 'grok', 'frak', 'blaster', 'klingon', 'shepherd']
 
+// const result = words1.filter(r => (r.length)>7 ? r :"");
+
+const result = words1.filter(r => (r.length)>7);    /// Acts like real filter
+
+
+console.log(result);                 /// o/p  : [ 'shepherd' ]
+// console.log(words1[5]);              //  o/p  : shepherd
 // expected output: Array ['shepherd']
 
 // ----------------------------------------------------------
 // 2. Find all even values
-const words = [12, 13, 14, 15, 16, 17]
+const words2 = [12, 13, 14, 15, 16, 17]
 
 // expected output: Array [12, 14, 16]
+
+const result1 = words2.filter(r => (r%2===0));
+
+console.log(result1);
 
 // ----------------------------------------------------------
 // REAL LIFE EXAMPLES
@@ -41,6 +52,10 @@ const hunters = [
         status: 'active',
     },
 ]
+
+const result2 = hunters.filter(r => r.status === 'active');
+
+console.log(result2);
 
 // expected output: Array [
 //     {
