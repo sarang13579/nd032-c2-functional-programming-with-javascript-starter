444c7f14808bd6abe7f2fc2a4a2cc5189cb19d95
Sarang <sarang13579@gmail.com>
2021-02-08 20:38:47 +0530

    Solution 1

--- fp_in_js/hof_1.js
+++ fp_in_js/hof_1.js
@@ -1,8 +1,13 @@
 // 1. Create a function that when passed as a callback to the Higher Order function provided, makes the final return the number's square plus two
+const x = 2;
 
 const addTwo = (callback) => {
-  return callback() + 2
+  return callback + 2
 }
+
+const result = addTwo(x*x);
+
+console.log(result);
 
 // 2. Create a Higher Order Function that could take this callback to return a greeting
 
@@ -10,5 +15,7 @@ const createFullName = (first_name, last_name, title) => {
   return `${title} ${first_name} ${last_name}`
 }
 
-
+const greet = createFullName('sarang', 'kakkoth', 'Mr')
 // you might notice that this function is less than ideal - that's great! We'll talk about why next
+
+console.log(greet);
